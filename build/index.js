5729baeb3590eb290d2dea5cf39ba7d857f86230
Bruno Rafael <brunrb96@gmail.com>
2023-11-28 18:17:53 -0300

    Nodejs update - 18:00 to 18:17 - 28/12/2022

--- build/index.js
+++ build/index.js
@@ -6,3 +6,23 @@ printaObjectos({
     name: "Bruno Rafael",
     vulgo: "Dev",
 });
+let ligado = false;
+let nome = "Bruno";
+let idade = 30;
+let altura = 1.9;
+let nulo = null;
+let indefinido = undefined;
+let retorno;
+function selectQuery() { }
+;
+let retornoView = false;
+let produto = {
+    name: "bruno",
+    cidade: "sp",
+    idade: "27",
+};
+let meuProduto = {
+    nome: "Tênis",
+    preco: 89.99,
+    unidades: 5,
+};
